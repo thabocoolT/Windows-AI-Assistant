7cc48cf3a36d6ff4ed2e8f4c358338670c240972
thabocoolT <Thaboprince252@gmail.com>
2026-06-19 19:54:36 +0200

    version 0.1(update)

--- assistant/speaker.py
+++ assistant/speaker.py
@@ -1,13 +1,9 @@
 import pyttsx3
 
-
-engine=pyttsx3.init()
-engine.setProperty(
-    "rate", 180
-)
-
-
 def speak(message):
     print(f"Assistant: {message}")
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 180)
     engine.say(message)
     engine.runAndWait()
+    engine.stop()
